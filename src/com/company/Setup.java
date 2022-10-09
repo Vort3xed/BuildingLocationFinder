402f124de171db3b621467014001e685c0a65819
HuangEric <66891965+HuangEric34@users.noreply.github.com>
2022-10-09 14:41:14 -0400

    final setup for submission

--- src/com/company/Setup.java
+++ src/com/company/Setup.java
@@ -1,21 +1,12 @@
 package com.company;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.scene.Group;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.*;
-import javafx.stage.Stage;
-
-import java.io.*;
-import java.util.concurrent.atomic.AtomicBoolean;
-
 
 public class Setup {
+
     public static void setLabel(Label name, String text, String style, double x, double y) {
         name.setText(text);
         name.setStyle(style);
@@ -36,4 +27,10 @@ public class Setup {
         name.setLayoutX(x);
         name.setLayoutY(y);
     }
+    
+    public static void setDropdown(ComboBox name, double x, double y){
+        name.setLayoutX(x);
+        name.setLayoutY(y);
+    }
+    
 }
