6cb4cc93b29dcdb450734a82284d2a3951d68288
HuangEric <66891965+HuangEric34@users.noreply.github.com>
2022-10-09 03:24:35 -0400

    completed login and admin page
    logo name disappears after buttons are pressed on admin page

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,12 +19,13 @@ import javafx.stage.Stage;
 import javafx.application.Application;
 import java.io.*;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Main extends Application {
     Label admin = new Label();
     Label welcome = new Label();
-    Label logoName = new Label();
 
     TextField adminUsername = new TextField();
     TextField adminPassword = new TextField();
@@ -32,12 +33,16 @@ public class Main extends Application {
     Button adminSubmit = new Button();
 
     Group groupHomePage = new Group();
-    Scene signInPage = new Scene(groupHomePage, 375, 600);
-
     Group groupAdminPage = new Group();
-    public Scene adminPage = new Scene(groupAdminPage, 375, 600);
+    Group groupEmployeeInt = new Group();
 
-    Label adminIdentifier = new Label();
+    Scene signInPage = new Scene(groupHomePage, 375, 600);
+    Scene adminPage = new Scene(groupAdminPage, 375, 600);
+    Scene employeeInterface = new Scene(groupEmployeeInt, 375, 600);
+
+    File users = new File("src/com/company/serverInternals/users.txt");
+    File rooms = new File("src/com/company/serverInternals/rooms.txt");
+    File events = new File("src/com/company/serverInternals/events.txt");
 
     Label addUsers = new Label();
     Label addLocations = new Label();
@@ -50,25 +55,28 @@ public class Main extends Application {
     Button returnToHome = new Button();
 
     Button enterUser = new Button();
-    Button enterLocation = new Button("Submit Location");
-    Button enterEvent = new Button("Submit Event");
+    Button enterLocation = new Button();
+    Button enterEvent = new Button();
 
-    File users = new File("src/com/company/serverInternals/users.txt");
-    File rooms = new File("src/com/company/serverInternals/rooms.txt");
-    File events = new File("src/com/company/serverInternals/events.txt");
+    Label logoName = new Label();
 
     public static void main(String[] args) {
         Application.launch(args);
     }
 
-    public void start(Stage stage){
+    public void start(Stage stage) {
         signInPage.setFill(new LinearGradient(
                 0, 0, 1, 1, true, CycleMethod.NO_CYCLE,
                 new Stop(0, Color.web("#5f0086")),
                 new Stop(1, Color.web("#010080")))
         );
 
-        signInPage.getStylesheets().add("https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap");       // image importing software, need to simplify
+        signInPage.getStylesheets().add("https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap");
+
+        stage.setScene(signInPage);
+        stage.setTitle("Team Track");
+        stage.setResizable(false);
+        stage.show();
 
         InputStream stream = null;
         try {
@@ -87,16 +95,9 @@ public class Main extends Application {
         imageView.setFitWidth(70);
         imageView.setPreserveRatio(true);
 
-        stage.setScene(signInPage);
-        stage.setTitle("Team Track");
-        stage.setResizable(false);
-        stage.show();
-
         int xSetupShift = 2;
-
         Setup.setLabel(welcome, "Welcome back.", "-fx-text-fill: E0FBFC; -fx-font-size: 24px; -fx-font-family: 'Montserrat', sans-serif;", 80 + xSetupShift, 100);
         Setup.setLabel(logoName, "TeamTrack", "-fx-text-fill: E0FBFC; -fx-font-size: 24px; -fx-font-family: 'Montserrat', sans-serif;", 60 + xSetupShift, 15);
-
         {
             Setup.setLabel(admin, "Login", "-fx-text-fill: E0FBFC; -fx-font-size: 18px; -fx-font-family: 'Montserrat', sans-serif;", 150 + xSetupShift, 153);
             Setup.setTextField(adminUsername, "Username", false, 105 + xSetupShift, 185);
@@ -106,15 +107,15 @@ public class Main extends Application {
 
         AtomicBoolean adminPageGenerated = new AtomicBoolean(false);
         adminSubmit.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
+            //String userString = Files.readAllBytes(Paths.get("src/com/company/serverInternals/users.txt"));
             if (adminUsername.getText().equals("r") && adminPassword.getText().equals("r")) {
                 if (!adminPageGenerated.get()) {
-                    generateAdminPage(stage);
+                    generateAdminPage();
                     adminPageGenerated.set(true);
                 }
                 stage.setScene(adminPage);
             } else {
                 admin.setText("Administrator Sign In: (Sign in failed!)");
-                admin.setLayoutX(15);
             }
         });
 
@@ -128,36 +129,19 @@ public class Main extends Application {
             stage.setScene(employeeInterface);
         });
          */
+        returnToHome.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
+            stage.setScene(signInPage);
+        });
 
-        groupHomePage.getChildren().addAll(welcome, admin, adminUsername, adminPassword, adminSubmit, logoName, imageView);
+        groupHomePage.getChildren().addAll(welcome, admin, adminUsername, adminPassword, adminSubmit, imageView, logoName);
     }
-    public void generateAdminPage(Stage stage) {
+    public void generateAdminPage() {
+        Label adminIdentifier = new Label();
         adminPage.setFill(new LinearGradient(
                 0, 0, 1, 1, true, CycleMethod.NO_CYCLE,
                 new Stop(0, Color.web("#5f0086")),
                 new Stop(1, Color.web("#010080")))
         );
-
-        int xOffset = -30;
-        {
-            Setup.setLabel(logoName, "TeamTrack", "-fx-text-fill: E0FBFC; -fx-font-size: 24px; -fx-font-family: 'Montserrat', sans-serif;", 62, 15);
-            Setup.setLabel(adminIdentifier, "Administrator Page", "-fx-text-fill: E0FBFC; -fx-font-size: 24px; -fx-font-family: 'Montserrat', sans-serif;", 115 + xOffset, 50);
-            Setup.setButton(returnToHome, "Return to Home Page", false, 360 + xOffset, 150);
-        }
-
-        {
-            Setup.setLabel(addUsers, "Add Users", "-fx-text-fill: E0FBFC; -fx-font: 18px 'San Francisco';", 130 + xOffset, 100);
-            Setup.setTextField(userBox, "Enter Employee Name", false, 100 + xOffset, 130);
-            Setup.setButton(enterUser, "Submit User", false, 100 + xOffset, 165);
-
-            Setup.setLabel(addLocations, "Add Locations", "-fx-text-fill: E0FBFC; -fx-font: 18px 'San Francisco';", 100 + xOffset, 180);
-            Setup.setTextField(locationBox, "Enter Room/Location", false, 100 + xOffset, 180);
-            Setup.setButton(enterLocation, "Submit Location", false, 100 + xOffset, 180);
-
-            Setup.setLabel(addEvents, "Add Events", "-fx-text-fill: E0FBFC; -fx-font: 18px 'San Francisco';", 100 + xOffset, 180);
-            Setup.setTextField(eventBox, "Enter Common Events", false, 100 + xOffset, 180);
-            Setup.setButton(enterEvent, "Submit Event", false, 100 + xOffset, 180);
-        }
 
         InputStream stream = null;
         try {
@@ -176,18 +160,34 @@ public class Main extends Application {
         imageView.setFitWidth(70);
         imageView.setPreserveRatio(true);
 
+
+        int xOffset = -30;
+        int yOffset = 20;
         {
-            enterUser.setOnAction(this::handleWriteToUsers);
-            enterLocation.setOnAction(this::handleWriteToLocations);
-            enterEvent.setOnAction(this::handleWriteToEvents);
+            Setup.setLabel(logoName, "TeamTrack", "-fx-text-fill: E0FBFC; -fx-font-size: 24px; -fx-font-family: 'Montserrat', sans-serif;", 62, 15);
+            Setup.setLabel(adminIdentifier, "Administrator Page", "-fx-text-fill: E0FBFC; -fx-font-size: 24px; -fx-font-family: 'Montserrat', sans-serif;", 90 + xOffset, 50 + yOffset);
+            Setup.setButton(returnToHome, "Return to Home Page", false, 151 + xOffset, 130 + yOffset);
+
+            Setup.setLabel(addUsers, "Add Users", "-fx-text-fill: E0FBFC; -fx-font-size: 18px; -fx-font-family: 'Montserrat', sans-serif;", 177 + xOffset, 180 + yOffset);
+            Setup.setTextField(userBox, "Enter Employee Name", false, 147 + xOffset, 210 + yOffset);
+            Setup.setButton(enterUser, "Submit User", false, 176 + xOffset, 237 + yOffset);
+
+            Setup.setLabel(addLocations, "Add Locations", "-fx-text-fill: E0FBFC; -fx-font-size: 18px; -fx-font-family: 'Montserrat', sans-serif;", 170 + xOffset, 270 + yOffset);
+            Setup.setTextField(locationBox, "Enter Room/Location", false, 150 + xOffset, 300 + yOffset);
+            Setup.setButton(enterLocation, "Submit Location", false, 179 + xOffset, 327 + yOffset);
+
+            Setup.setLabel(addEvents, "Add Events", "-fx-text-fill: E0FBFC; -fx-font-size: 18px; -fx-font-family: 'Montserrat', sans-serif;", 180 + xOffset, 360 + yOffset);
+            Setup.setTextField(eventBox, "Enter Common Events", false, 150 + xOffset, 390 + yOffset);
+            Setup.setButton(enterEvent, "Submit Event", false, 179 + xOffset, 417 + yOffset);
         }
 
-        returnToHome.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
-            stage.setScene(signInPage);
-        });
+        enterUser.setOnAction(this::handleWriteToUsers);
+        enterLocation.setOnAction(this::handleWriteToLocations);
+        enterEvent.setOnAction(this::handleWriteToEvents);
 
         groupAdminPage.getChildren().addAll(adminIdentifier, returnToHome, addUsers, addLocations, addEvents, userBox, locationBox,
-                eventBox, enterUser, enterLocation, enterEvent, welcome, logoName, imageView);
+                eventBox, enterUser, enterLocation, enterEvent, imageView, logoName);
+
     }
 
     public void handleWriteToEvents(ActionEvent event) {
@@ -232,3 +232,5 @@ public class Main extends Application {
         userBox.clear();
     }
 }
+
+
